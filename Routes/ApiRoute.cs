ee6ffbd42a03682faf9490c58ffcc621e3d56321
T-Terra <gabst138@gmail.com>
2025-04-05 16:31:32 -0300

    feat: add cancellationtoken in api routes

--- Routes/ApiRoute.cs
+++ Routes/ApiRoute.cs
@@ -9,27 +9,27 @@ public static class ApiRoute
     public static void ApiRoutes(this WebApplication app)
     {
         var route = app.MapGroup("api");
-        route.MapPost("add", async (ExpenseRequest req, ExpenseContext context) =>
+        route.MapPost("add", async (ExpenseRequest req, ExpenseContext context, CancellationToken ct) =>
         {
             // cria uma despesa
             var expense = new ExpensesModel(req.NameExpense, req.AmountExpense, req.DescriptionExpense, req.CategoryExpense);
             // adiciona no banco de dados
-            await context.AddAsync(expense);
+            await context.AddAsync(expense, ct);
             // Faz o commit no banco de dados
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(ct);
             
             return Results.Created("/api/add", expense);
         });
 
-        route.MapGet("list", async (ExpenseContext context) =>
+        route.MapGet("list", async (ExpenseContext context, CancellationToken ct) =>
         {
-            var expenses = await context.Expenses.ToListAsync();
+            var expenses = await context.Expenses.ToListAsync(ct);
             return Results.Ok(expenses);
         });
 
-        route.MapPut("update/{id:guid}", async (Guid id, ExpenseRequest req, ExpenseContext context) =>
+        route.MapPut("update/{id:guid}", async (Guid id, ExpenseRequest req, ExpenseContext context, CancellationToken ct) =>
         {
-            var expense = await context.Expenses.FirstOrDefaultAsync(x => x.Id == id);
+            var expense = await context.Expenses.FirstOrDefaultAsync(x => x.Id == id, ct);
             
             if (expense == null)
             {
@@ -37,14 +37,14 @@ public static class ApiRoute
             }
             
             expense.ChangeValues(req.NameExpense, req.AmountExpense, req.DescriptionExpense, req.CategoryExpense);
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(ct);
             
             return Results.Ok(expense);
         });
 
-        route.MapDelete("delete/{id:guid}", async (Guid id, ExpenseContext context) =>
+        route.MapDelete("delete/{id:guid}", async (Guid id, ExpenseContext context, CancellationToken ct) =>
         {
-            var expense = await context.Expenses.FirstOrDefaultAsync(x => x.Id == id);
+            var expense = await context.Expenses.FirstOrDefaultAsync(x => x.Id == id, ct);
 
             if (expense == null)
             {
@@ -52,7 +52,7 @@ public static class ApiRoute
             }
 
             context.Expenses.Remove(expense);
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync(ct);
 
             return Results.Ok(expense);
         });
