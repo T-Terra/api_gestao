f1040545a87161b0ec2e23ea74e7260e80ce9ac1
T-Terra <gabst138@gmail.com>
2025-04-15 21:02:37 -0300

    feat: add configs deploy

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ExpenseContext>();
