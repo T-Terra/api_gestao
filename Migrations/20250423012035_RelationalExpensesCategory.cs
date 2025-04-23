0a4accfb1791f63502f395e201f4bc11724421f3
T-Terra <gabst138@gmail.com>
2025-04-22 23:13:01 -0300

    feat: add migrations relational categories to expenses

---
+++ Migrations/20250423012035_RelationalExpensesCategory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expenses.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationalExpensesCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_CategoryId",
+                table: "Expenses",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Categories_CategoryId",
+                table: "Expenses",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Categories_CategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_CategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Expenses");
+        }
+    }
+}
